e96846256fb697c8d3311d9854e9795d64d8a049
ms <matussobon@gmail.com>
2025-11-24 18:26:36 +0100

    hit_sphere function

---
+++ objects.py
@@ -0,0 +1,12 @@
+import math
+
+def hit_sphere(center, radius, ray):
+    """
+    TODO:
+    - compute oc = ray.origin - center
+    - compute a, half_b, c for the quadratic equation
+    - compute discriminant
+    - return closest valid hit t or None
+    """
+    #return None
+    pass
