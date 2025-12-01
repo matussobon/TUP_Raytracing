8c4898a8a319921e0b5ebe47fee894e8e8222400
ms <matussobon@gmail.com>
2025-12-01 19:07:42 +0100

    simpler discriminant

--- objects.py
+++ objects.py
@@ -1,12 +1,15 @@
 import math
 
 def hit_sphere(center, radius, ray):
-    """
-    TODO:
-    - compute oc = ray.origin - center
-    - compute a, half_b, c for the quadratic equation
-    - compute discriminant
-    - return closest valid hit t or None
-    """
-    #return None
-    pass
+    oc = center - ray.origin
+
+    a = ray.direction.dot(ray.direction)
+    b = -2*(ray.direction.dot(oc))
+    c = oc.dot(oc) - radius**2
+    h = ray.direction.dot(oc)
+
+    discriminant = h**2-a*c
+
+    if discriminant < 0:
+        return -1.0
+    else: return (h - math.sqrt(discriminant))/a
