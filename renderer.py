a421834d5929b9ccb024aedb2684a90d6420a7f3
ms <matussobon@gmail.com>
2025-12-01 19:09:25 +0100

    sphere shading

--- renderer.py
+++ renderer.py
@@ -2,14 +2,15 @@ from vec3 import *
 from objects import *
 
 
-def ray_color(ray):
-    # Sphere parameters
-    center = Vec3(0, 0, -2)
-    radius = 0.5
-    t_hit = hit_sphere(center, radius, ray)
-    if t_hit is not None:
-        return Vec3(1,0,0)
+def ray_color(ray, sphere_center, sphere_radius):
 
+    t_hit = hit_sphere(sphere_center, sphere_radius, ray)
+
+    if t_hit > 0:
+        sphere_normal = (ray.at(t_hit) - sphere_center).unit()
+        return Vec3(0.5*(sphere_normal.x + 1.0), 0.5*(sphere_normal.y + 1.0), 0.5*(sphere_normal.z + 1.0))
+    # if t_hit is not None:
+    #     return Vec3(1,0,0)
 
     # background gradient
     unit_dir = ray.direction.unit()
